229ab22b8ad39889f5fe0b13f7380048be5e5a21
nrjGH <waniraj2810@gmail.com>
2024-01-07 21:59:16 +0530

    added apierror,apiresponse in util, user and video models

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+const app = express()
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+app.use(express.json({limit:"16kb"}))                       //to ensure json packages do not exceed 16kb
+app.use(express.urlencoded({extended:true, limit:"16kb"}))  //to ensure URL be accepted in various forms
+app.use(express.static("public"))                           //to store static data
+app.use(cookieParser())                                     //cookieparser helps execute crud operations on cookies in user's browser 
+export {app}
---
+++ src/models/user.model.js
@@ -0,0 +1,85 @@
+import mongoose,{Schema} from "mongoose"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+const userSchema = Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,              // trims off the spaces before or after
+        index:true              // it is better to add index to the entities which will be searched a lot in the database
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+    },
+    fullName:{
+        type:String,
+        required:true,
+        index:true
+    },
+    avatar:{
+        type:String,             //cloudinary url
+        required:true
+    },
+    coverImage:{
+        type:String
+    },
+    watchHistory:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Video"
+        }
+    ],
+    password:{
+        type:String,
+        required:[true,'Password is required']    
+    },
+    refreshToken:{
+        type:String
+    },s
+    
+
+},{timestamps:true})
+
+userSchema.pre("save",async function (next){            // pre hooks are used to run any code executions just before saving
+    if(! this.isModified("password")) return next();    // only if "passsword" field is modified, we want the pre hook to work, else we can straight away call the next()
+    this.password=bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (passsword){
+    return await bcrypt.compare(passsword,this.passsword)
+}
+
+userSchema.methods.generateAccesToken = function(){
+    jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullName:this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY,
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    jwt.sign(
+        {
+            _id:this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY,
+        }
+    )
+}
+
+export const User = mongoose.model("User",userSchema)
