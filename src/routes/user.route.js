9749b9f4e33fe67fe895c88b355526808b6b3f72
nrjGH <waniraj2810@gmail.com>
2024-01-31 23:59:40 +0530

    aggregation pipelines written  and routes added

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken} from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, getUserChannelProfile, getWatchHistory} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import  verify  from "jsonwebtoken";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -24,6 +24,20 @@ router.route("/login").post(loginUser)
 router.route("/logout").get(verifyJWT, logoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+router.route("/history").get(verifyJWT, getWatchHistory)
+
+
 
 
 export default router
